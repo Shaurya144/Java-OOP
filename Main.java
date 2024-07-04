d60e9b716651d837a70f3f72e5c187d628a72779
Shaurya144 <chundawatshaurya@gmail.com>
2024-07-04 20:44:05 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,21 @@
+public class Main {
+
+    int variable = 5;
+
+    public static void main(String[] args) {
+        // OOP stands for Object-Oriented Programming.
+        // OOP is about creating objects that contain both data and methods
+        // What are Classes and Objects?
+
+        // Classes and Objects are examples of OOP
+
+        // Classes are a template
+        // Objects are an instance / example of that template
+        // In other words, templates are broad or general
+
+        // ----------------- Classes -----------------------
+        Main myObject = new Main(); // Creates a myObject Object
+        System.out.println(myObject.variable);
+
+    }
+}
